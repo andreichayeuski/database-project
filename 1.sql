e5a92e5057cdfe5eae4190a86d55d28268df2c07
Kiril <ermakovkiril@gmail.com>
2019-02-19 13:07:44 +0300

    Master
    first push

---
+++ 1.sql
@@ -0,0 +1,343 @@
+use Arenda;
+drop table Clients;
+drop table Cars;
+drop table Contracts;
+drop table DTP
+
+create table Clients(
+Id_client int IDENTITY(1,1) primary key,
+Name_client nvarchar(50),
+Passport_client nvarchar(50),
+Experience_year_client int,
+Telephone_client nvarchar(10),
+Address_client nvarchar(50)
+)
+
+create table Cars(
+Id_car int IDENTITY(1,1) primary key,
+Marka_car nvarchar(50),
+Color_car nvarchar(50),
+Status_car nvarchar(50)
+)
+
+create table Contracts(
+Id_contract int IDENTITY(1,1) primary key,
+Date_arenda_start date,
+Date_arenda_end date,
+Client int,
+Car int,
+Cost_arenda int,
+FOREIGN KEY (Client) REFERENCES Clients(Id_client),
+FOREIGN KEY (Car) REFERENCES Cars(Id_car)
+)
+
+create table DTP(
+Id_dtp int IDENTITY(1,1) primary key,
+Date_dtp date,
+Id_contract int,
+Id_car int,
+Procent_broken int,
+About_dtp varchar(50),
+CHECK (Procent_broken between 1 and 100),
+FOREIGN KEY (Id_contract) REFERENCES Contracts(Id_contract),
+FOREIGN KEY (Id_car) REFERENCES Cars(Id_car)
+)
+
+
+insert into Clients values('Petya','RR12345678',3,'147-12-92','Ymanskaya 19');
+insert into Clients values('Alla','RR12345687',1 ,'159-29-49','Katachatova 12');
+insert into Clients values('Anastasya','RR12345687',2,'147-87-74','Azbekova 8');
+insert into Clients values('Nikolay','RR1475236',5 ,'156-23-34','Armena 121');
+insert into Clients values('Vasiliy','RR87654321',6 ,'785-63-98','Aleva 23');
+
+select * from Clients;
+
+Insert into Cars values('Toyota RAV4','Blue','New');
+Insert into Cars values('Toyota Camry','White','Old');
+Insert into Cars values('Fiat Punto','Black','Old, crashed');
+Insert into Cars values('Honda Civic','Orange','old');
+Insert into Cars values('Kia Rio','Black','New');
+
+select * from Cars;
+
+Insert into Contracts values('17-02-2019','25-02-2019', 5,5 ,300);
+Insert into Contracts values('17-03-2018','25-03-2018', 5,4 ,120);
+Insert into Contracts values('11-02-2019','21-02-2019', 3,2 ,145);
+Insert into Contracts values('01-02-2019','07-02-2019', 2,1 ,230);
+Insert into Contracts values('03-02-2019','13-02-2019', 2,5 ,500);
+Insert into Contracts values('18-02-2019','20-02-2019', 3,3 ,300);
+Insert into Contracts values('21-02-2019','24-02-2019', 2,3 ,250);
+Insert into Contracts values('25-02-2019','28-02-2019', 3,3 ,100);
+Insert into Contracts values('16-02-2019','17-02-2019', 2,2 ,200);
+
+select * from Contracts;
+
+Insert into DTP values('20-02-2019',6,3,10,'Pozarapal krylo');
+Insert into DTP values('24-02-2019',7,3,20,'Pozarapal vtoroe krylo');
+Insert into DTP values('28-02-2019',8,3,95,'Razbila');
+Insert into DTP values('17-02-2019',9,2,10,'Pozarapala krylo');
+
+select * from DTP;
+--------------------------------------------------------------------------------------------------------------------------
+
+GO
+CREATE INDEX date_begin_end_index
+ON Contracts(Date_arenda_start, Date_arenda_end)
+
+GO
+CREATE INDEX name__index
+ON Clients(name_client)
+
+--------------------------
+
+CREATE VIEW Contract_view AS SELECT Contracts.Date_arenda_start, Contracts.Date_arenda_end, Clients.Name_client,
+Cars.Marka_car, Contracts.Cost_arenda FROM ((Contracts
+INNER JOIN  Clients ON Contracts.Client = Clients.Id_client)
+INNER JOIN  Cars ON Contracts.Car = Cars.Id_car);
+
+select * from Contract_view;
+
+--------------------------
+
+GO  
+CREATE FUNCTION countContractsOfClient(@datastarta date,@dataend date)  
+RETURNS int   
+AS   
+BEGIN  
+    DECLARE @ret int;  
+    SELECT @ret =count(*)   
+    FROM Contracts co 
+    WHERE Date_arenda_start between @datastarta and @dataend;  
+     IF (@ret IS NULL)   
+        SET @ret = 0;  
+    RETURN @ret;  
+END;  
+GO  
+
+  drop function dbo.countContractsOfClient;
+  select dbo.countContractsOfClient('01-02-2019','10-02-2019');
+
+-----------------------
+GO  
+CREATE PROCEDURE ContractsOfClient(@datastarta date,@dataend date)    
+AS   
+BEGIN  
+    SELECT *   
+    FROM Contracts co 
+    WHERE Date_arenda_start between @datastarta and @dataend;  
+END;  
+GO  
+
+drop function dbo.ContractsOfClient;
+  exec ContractsOfClient '01-02-2019','10-02-2019';
+  
+-----------------------
+CREATE TABLE History 
+(
+    Id INT IDENTITY PRIMARY KEY,
+    Operation NVARCHAR(200) NOT NULL,
+    CreateAt DATETIME NOT NULL DEFAULT GETDATE(),
+);
+
+drop table History;
+drop trigger Car_INSERT;
+
+GO
+CREATE TRIGGER Car_INSERT
+on Cars
+AFTER INSERT
+AS
+INSERT INTO History(Operation)
+SELECT  'Добавлена машина' + Marka_car + '   цвета ' + Color_car + ' Статуса ' + Status_car FROM inserted
+
+Insert into Cars values('Lada Vesta','Gray','New');
+select * from History;
+
+
+------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------
+drop procedure add_client
+
+CREATE PROCEDURE add_client
+    @fio nvarchar(50),
+	@passport nvarchar (max),
+	@experience nvarchar(15),
+	@tel nvarchar(50),
+	@adr nvarchar(50)
+AS
+	BEGIN
+		INSERT INTO Clients(Name_client,Passport_client,Experience_year_client,Telephone_client,Address_client)
+		values(	@fio,@passport,@experience,@tel,@adr)
+		DECLARE @userId int;
+		SELECT 0;
+	END
+GO
+----
+CREATE PROCEDURE drop_client
+	@passport nvarchar (max)
+AS
+	BEGIN
+	DELETE FROM Clients where Passport_client = @passport;
+		SELECT 0;
+	END
+GO
+----
+CREATE PROCEDURE change_client
+    @fio nvarchar(50),
+	@passport nvarchar (max),
+	@experience nvarchar(15),
+	@tel nvarchar(50),
+	@adr nvarchar(50)
+AS
+	BEGIN
+	update Clients set Name_client=@fio, Experience_year_client=@experience,Telephone_client=@tel,Address_client=@adr 
+	where Passport_client=@passport;
+		SELECT 0;
+	END
+GO
+----
+create procedure  getAllClients
+AS
+select * from Clients
+
+-------------------------------------------------------------------------------------
+
+
+CREATE PROCEDURE add_car
+    @marka nvarchar(50),
+	@color nvarchar (max),
+	@status nvarchar(15)
+AS
+	BEGIN
+		INSERT INTO Cars(Marka_car,Color_car,Status_car)
+		values(	@marka,@color,@status)
+		SELECT 0;
+	END
+GO
+----
+CREATE PROCEDURE drop_car
+	@marka nvarchar (max)
+AS
+	BEGIN
+	DELETE FROM Cars where Marka_car = @marka;
+		SELECT 0;
+	END
+GO
+----
+CREATE PROCEDURE change_car
+     @marka nvarchar(50),
+	@color nvarchar (max),
+	@status nvarchar(15)
+AS
+	BEGIN
+	update Cars set Color_car=@color,Status_car=@status 
+	where Marka_car=@marka;
+		SELECT 0;
+	END
+GO
+----
+create procedure  getAllCars
+AS
+select * from Cars
+-------------------------------------------------------------------------------
+CREATE PROCEDURE add_contract
+    @datestart date,
+	@dateend date,
+	@client int,
+	@car int,
+	@cost int
+AS
+	BEGIN
+		INSERT INTO Contracts(Date_arenda_start,Date_arenda_end,Client,Car,Cost_arenda)
+		values(	@datestart,@dateend,@client,@car,@cost)
+		SELECT 0;
+	END
+GO
+----
+CREATE PROCEDURE drop_contract
+	 @datestart date,
+	@dateend date
+AS
+	BEGIN
+	DELETE FROM Contracts where Date_arenda_start between @datestart and @dateend;
+		SELECT 0;
+	END
+GO
+----
+CREATE PROCEDURE change_contract
+      @datestart date,
+	@dateend date,
+	@client int,
+	@car int,
+	@cost int
+AS
+	BEGIN
+	update Contracts set Date_arenda_start=@datestart,Date_arenda_end=@dateend,Car=@car,Cost_arenda=@cost 
+	where Client=@client;
+		SELECT 0;
+	END
+GO
+---
+create procedure  getallContracts
+AS
+SELECT Contracts.Date_arenda_start, Contracts.Date_arenda_end, Clients.Name_client,
+Cars.Marka_car, Contracts.Cost_arenda FROM ((Contracts
+INNER JOIN  Clients ON Contracts.Client = Clients.Id_client)
+INNER JOIN  Cars ON Contracts.Car = Cars.Id_car);
+
+-----------------------------------------------------------------------------------------------------------------------
+
+CREATE PROCEDURE add_dtp
+    @datedtp date,
+	@contract int,
+	@car int,
+	@procent int,
+	@about varchar(50)
+AS
+	BEGIN
+		INSERT INTO DTP(Date_dtp,Id_contract,Id_car,Procent_broken,About_dtp)
+		values(	@datedtp,@contract,@car,@procent,@about)
+		SELECT 0;
+	END
+GO
+----
+CREATE PROCEDURE drop_dtp
+	@contract int
+AS
+	BEGIN
+		DELETE FROM DTP where Id_contract = @contract
+		SELECT 0;
+	END
+GO
+----
+CREATE PROCEDURE change_dtp
+    @datedtp date,
+	@contract int,
+	@car int,
+	@procent int,
+	@about varchar(50)
+AS
+	BEGIN
+	update DTP set Date_dtp=@datedtp,Id_car=@car,Procent_broken=@procent,About_dtp=@about 
+	where Id_contract=@contract;
+		SELECT 0;
+	END
+GO
+----
+create procedure  getAllDtp
+AS
+select * from DTP
+-----------------------
+
+CREATE PROCEDURE spisok_arend
+    @datestart date,
+	@dateend date
+AS
+	BEGIN
+SELECT Contracts.Date_arenda_start, Contracts.Date_arenda_end, Clients.Name_client,
+Cars.Marka_car, Contracts.Cost_arenda FROM ((Contracts
+INNER JOIN  Clients ON Contracts.Client = Clients.Id_client)
+INNER JOIN  Cars ON Contracts.Car = Cars.Id_car) ORDER BY Contracts.Cost_arenda asc;
+	END
+GO
---
+++ 2lab/2lab/DB.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace _2lab
+{
+    class DB
+    {
+        SqlConnection conn;
+        public void openConnection(string connStr)
+        {
+            conn = new SqlConnection(connStr);
+            conn.Open();
+        }
+
+        public void closeConnection()
+        {
+            conn.Close();
+        }
+
+        public void add_client(string fio, string passport, int experience, string tel, string adr)
+        {
+            using (SqlCommand cmd = new SqlCommand("add_client", conn))
+            {
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@fio", fio);
+                cmd.Parameters.AddWithValue("@passport", passport);
+                cmd.Parameters.AddWithValue("@experience", experience);
+                cmd.Parameters.AddWithValue("@tel", tel);
+                cmd.Parameters.AddWithValue("@adr", adr);
+                cmd.ExecuteNonQuery();
+            }
+
+        }
+        public void drop_client(string passport)
+        {
+            using (SqlCommand cmd = new SqlCommand("drop_client", conn))
+            {
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@passport", passport);
+                cmd.ExecuteNonQuery();
+            }
+
+        }
+
+        public void change_client(string fio, string passport, int experience, string tel, string adr)
+        {
+            using (SqlCommand cmd = new SqlCommand("change_client", conn))
+            {
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@fio", fio);
+                cmd.Parameters.AddWithValue("@passport", passport);
+                cmd.Parameters.AddWithValue("@experience", experience);
+                cmd.Parameters.AddWithValue("@tel", tel);
+                cmd.Parameters.AddWithValue("@adr", adr);
+                cmd.ExecuteNonQuery();
+            }
+
+        }
+
+        // ----------------------------------------------------------------------------
+
+        public void add_car(string marka, string color, string status)
+        {
+            using (SqlCommand cmd = new SqlCommand("add_car", conn))
+            {
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@marka", marka);
+                cmd.Parameters.AddWithValue("@color", color);
+                cmd.Parameters.AddWithValue("@status", status);
+                cmd.ExecuteNonQuery();
+            }
+
+        }
+
+        public void drop_car(string marka)
+        {
+            using (SqlCommand cmd = new SqlCommand("drop_car", conn))
+            {
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@marka", marka);
+                cmd.ExecuteNonQuery();
+            }
+
+        }
+
+        public void change_car(string marka, string color, string status)
+        {
+            using (SqlCommand cmd = new SqlCommand("change_car", conn))
+            {
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@marka", marka);
+                cmd.Parameters.AddWithValue("@color", color);
+                cmd.Parameters.AddWithValue("@status", status);
+                cmd.ExecuteNonQuery();
+            }
+
+        }
+
+        //------------------------------------------------------------------
+
+        public void add_contract(DateTime datestart, DateTime dateend, int client, int car, int cost)
+        {
+            using (SqlCommand cmd = new SqlCommand("add_contract", conn))
+            {
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@datestart", datestart);
+                cmd.Parameters.AddWithValue("@dateend", dateend);
+                cmd.Parameters.AddWithValue("@client", client);
+                cmd.Parameters.AddWithValue("@car", car);
+                cmd.Parameters.AddWithValue("@cost", cost);
+                cmd.ExecuteNonQuery();
+            }
+
+        }
+
+        public void drop_contract(DateTime datestart, DateTime dateend)
+        {
+            using (SqlCommand cmd = new SqlCommand("drop_contract", conn))
+            {
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@datestart", datestart);
+                cmd.Parameters.AddWithValue("@dateend", dateend);
+                cmd.ExecuteNonQuery();
+            }
+
+        }
+        public void change_contract(DateTime datestart, DateTime dateend, int client, int car, int cost)
+        {
+            using (SqlCommand cmd = new SqlCommand("change_contract", conn))
+            {
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@datestart", datestart);
+                cmd.Parameters.AddWithValue("@dateend", dateend);
+                cmd.Parameters.AddWithValue("@client", client);
+                cmd.Parameters.AddWithValue("@car", car);
+                cmd.Parameters.AddWithValue("@cost", cost);
+                cmd.ExecuteNonQuery();
+            }
+
+        }
+        //---------------------------------------------------------------------------------------------------
+
+        public void add_dtp(DateTime datedtp,  int contract, int car,int proc, string about)
+        {
+            using (SqlCommand cmd = new SqlCommand("add_dtp", conn))
+            {
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@datedtp", datedtp);
+                cmd.Parameters.AddWithValue("@contract", contract);
+                cmd.Parameters.AddWithValue("@car", car);
+                cmd.Parameters.AddWithValue("@procent", proc);
+                cmd.Parameters.AddWithValue("@about", about);
+                cmd.ExecuteNonQuery();
+            }
+
+        }
+
+        public void drop_dtp(int contract)
+        {
+            using (SqlCommand cmd = new SqlCommand("drop_dtp", conn))
+            {
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@contract", contract);
+                cmd.ExecuteNonQuery();
+            }
+
+        }
+
+        public void change_dtp(DateTime datedtp, int contract, int car,int proc, string about)
+        {
+            using (SqlCommand cmd = new SqlCommand("change_dtp", conn))
+            {
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@datedtp", datedtp);
+                cmd.Parameters.AddWithValue("@contract", contract);
+                cmd.Parameters.AddWithValue("@car", car);
+                cmd.Parameters.AddWithValue("@procent", proc);
+                cmd.Parameters.AddWithValue("@about", about);
+                cmd.ExecuteNonQuery();
+            }
+
+        }
+
+    }
+}
